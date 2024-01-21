2bbd6d909fa61e0433af41306c6950c8ef265ca6
Sophi693 <sofiarojas7455@gmail.com>
2024-01-21 00:00:55 -0300

    subi  cambios

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path =require ('path')
 const port= 3030;
+
 app.listen(port,()=>console.log(`http://localhost:${port}`))
 app.use(express.static(path.join(__dirname, './public')));
 
